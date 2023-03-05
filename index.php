c56805e2b6a2c36d90fe79355b39ce737de7e9e8
Francesco98Welcome <frankbenvenuti04@gmail.com>
2023-03-05 12:55:46 +0100

    Esercizio √

---
+++ index.php
@@ -0,0 +1,69 @@
+<?php
+
+class Movie
+{
+    private $title;
+    public $year;
+    public $genre;
+
+    public function setTitle($title)
+    {
+        if (is_numeric($title)) {
+            echo 'error';
+        } else {
+            $this->title = $title;
+        }
+    }
+
+    function __construct($_year)
+    {
+        $this->year = $_year;
+    }
+}
+
+$avatar = new Movie(2010);
+$avatar->setTitle('AVATAR');
+echo '<h2>Titolo del film:</h2>';
+var_dump($avatar);
+echo ' -- IL FILM SI CHIAMA: AVATAR';
+
+echo '<br>';
+echo '<br>';
+
+$avatar = new Movie(2010);
+echo '<h2>Anno di pubblicazione:</h2>';
+var_dump($avatar);
+echo '-- IL FILM É USCITO NEL: ';
+echo $avatar->year;
+
+echo '<br>';
+echo '<br>';
+echo '<hr>';
+
+echo '<h1>Altro esempio fatto meglio </h1>';
+
+class MovieTwo
+{
+    public $title;
+    public $year;
+    public $genre;
+
+    function __construct($title, $year, $genre)
+    {
+        $this->title = $title;
+        $this->year = $year;
+        $this->genre = $genre;
+    }
+
+    function getDetails()
+    {
+        return "Titolo: " . $this->title . ", Anno di pubblicazione: " . $this->year . ", Genere: " . $this->genre . '<br>';
+    }
+}
+
+$movieOne = new MovieTwo("Avatar", 2010, "Azione/Fantascienza");
+$movieTwo = new MovieTwo("Titanic", 1998, "Romantic");
+
+
+echo $movieOne->getDetails();
+echo $movieTwo->getDetails();
